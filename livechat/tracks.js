fd089a14170eb983269c202fce6a0c864fb0ae05
sjafernik <65808085+sjafernik@users.noreply.github.com>
2022-07-26 08:53:46 -0400

    adding tracks and .iml file

---
+++ livechat/tracks.js
@@ -0,0 +1,26 @@
+var LC_API = LC_API || {};
+
+LC_API.on_chat_started = function(data)
+{
+};
+
+LC_API.on_chat_window_opened = function(data)
+{
+
+}
+
+LC_API.on_before_load = function (data) {
+
+}
+if(vls())
+{
+window.__lc = window.__lc || {};
+window.__lc.license = 14338644;
+
+
+(function() {
+  var lc = document.createElement('script'); lc.type = 'text/javascript'; lc.async = true;
+  lc.src = ('https:' == document.location.protocol ? 'https://' : 'http://') + 'cdn.livechatinc.com/staging/tracking.js';
+  var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(lc, s);
+})();
+}
